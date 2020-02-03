7ea568a70ac3bfd4b28aff5033b314ae241a8d0a
Sachko-Sergey <sachkosergey11@gmail.com>
2020-02-03 16:25:05 +0300

    artweld

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$('.slider').slick({
+  prevArrow: '<button type="button" class="slick-prev  controls-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next  controls-next">Previous</button>'
+});
+
+$('.partners-slider').slick({
+  prevArrow: '<button type="button" class="slick-prev  controls-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next  controls-next">Previous</button>',
+  slidesToShow: 5,
+  dots: true,
+  dotsClass: 'dots'
+});
+
+$('.popular__slider').slick({
+  prevArrow: '<button type="button" class="slick-prev  controls-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next  controls-next">Previous</button>',
+  slidesToShow: 3,
+  dots: true,
+  dotsClass: 'dots'
+});
+
+$('.articles__slider').slick({
+  prevArrow: '<button type="button" class="slick-prev  controls-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next  controls-next">Previous</button>',
+  slidesToShow: 3,
+  dots: true,
+  dotsClass: 'dots'
+});
